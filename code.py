f5944b9027f4f29359716711e06b09499df5f855
matsuru-hoshi <54957113+matsuru-hoshi@users.noreply.github.com>
2019-10-02 14:49:20 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Sep 2019
+# This program draws a background on the PyBadge
+
+import ugame
+import stage
+
+# an image bank for CircuitPython
+image_bank_1 = stage.Bank.from_bmp16("space_aliens.bmp")
+sprites = []
+def main():
+    # this function sets the background
+
+    # sets the background to image 0 in the bank
+    # backgrounds do not have magenta as a transparent color
+    background = stage.Grid(image_bank_1, 10, 8)
+    # changes (0,0) image to the 3rd image
+
+    ship = stage.Sprite(image_bank_1, 5, 75, 56)
+    sprites.append(ship)
+    alien = stage.Sprite(image_bank_1, 9, 64, 56)
+    sprites.append(alien)
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the background layer
+    game.layers = [background]
+    # render the background
+    # most likely you will only render background once per scene
+    game.render_block()
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass
+
+
+if __name__ == "__main__":
+    main()
